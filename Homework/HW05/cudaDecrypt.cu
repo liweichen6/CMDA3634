a2dabbce6fd61bf82e7f970d7418bc02f8bc2c57
Weichen Li <wli2@vt.edu>
2018-04-21 20:29:39 -0400

    HW05 Bonus

--- Homework/HW05/cudaDecrypt.cu
+++ Homework/HW05/cudaDecrypt.cu
@@ -7,6 +7,48 @@
 #include "cuda.h"
 #include "functions.c"
 
+/* Q4 Make the search for the secret key parallel on the GPU using CUDA. */
+
+//compute a*b mod p safely
+__device__ unsigned int kmodprod(unsigned int a, unsigned int b, unsigned int p) {
+    unsigned int za = a;
+    unsigned int ab = 0;
+    
+    while (b > 0) {
+        if (b%2 == 1) ab = (ab +  za) % p;
+        za = (2 * za) % p;
+        b /= 2;
+    }
+    return ab;
+}
+
+ //compute a^b mod p safely
+__device__ unsigned int kmodExp(unsigned int a, unsigned int b, unsigned int p) {
+    unsigned int z = a;
+    unsigned int aExpb = 1;
+    
+    while (b > 0) {
+        if (b%2 == 1) aExpb = kmodprod(aExpb, z, p);
+        z = kmodprod(z, z, p);
+        b /= 2;
+    }
+    return aExpb;
+}
+
+__global__ void kernelCompare(unsigned int *p, unsigned int *g, unsigned int *h, unsigned int *x) {
+  unsigned int threadId = threadIdx.x;
+  unsigned int blockId = blockIdx.x;
+  unsigned int nBlock = blockDim.x;
+
+  unsigned int id = threadId + blockId * nBlock;
+
+  if (id < *p - 1) {
+    if (kmodExp(*g, id + 1, *p) == *h) {
+      *x = id + 1;
+      printf("Secret key found! x = %u \n", *x);
+    }
+  }
+}
 
 int main (int argc, char **argv) {
 
@@ -53,16 +95,16 @@ int main (int argc, char **argv) {
     cudaMalloc(&d_h, sizeof(unsigned int));
     cudaMalloc(&d_x, sizeof(unsigned int));
     
-    cudaMemcpy(d_p, p, sizeof(unsigned int),cudaMemcpyHostToDevice);
-    cudaMemcpy(d_g, g, sizeof(unsigned int),cudaMemcpyHostToDevice);
-    cudaMemcpy(d_h, h, sizeof(unsigned int),cudaMemcpyHostToDevice);
+    cudaMemcpy(d_p, &p, sizeof(unsigned int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_g, &g, sizeof(unsigned int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_h, &h, sizeof(unsigned int), cudaMemcpyHostToDevice);
 
     int Nthreads = 1024;
     int Nblocks = (p + Nthreads - 2)/Nthreads;
     kernelCompare<<<Nblocks, Nthreads>>>(d_p, d_g, d_h, d_x);
 
     cudaDeviceSynchronize();
-    cudaMemcpy(x, d_x, sizeof(unsigned int),cudaMemcpyDeviceToHost);
+    cudaMemcpy(&x, d_x, sizeof(unsigned int), cudaMemcpyDeviceToHost);
 
     cudaFree(d_p);
     cudaFree(d_g);
@@ -85,22 +127,6 @@ int main (int argc, char **argv) {
   ElGamalDecrypt(Zmessage, a, Nints, p, x);
   convertZToString(Zmessage, Nints, message, Nints * (n - 1) / 8);
   printf("%s\n", message);
-
-  /* Q4 Make the search for the secret key parallel on the GPU using CUDA. */
-  __global__ void kernelCompare(unsigned int p, unsigned int g, unsigned int h, unsigned int x) {
-    int threadId = threadIdx.x;
-    int blockId = blockIdx.x;
-    int nBlock = blockDim.x;
-
-    int id = threadId + blockId * nBlock;
-
-    if (id < p - 1) {
-        if (modExp(g, id + 1, p) == h) {
-            x = id + 1;
-            printf("Secret key found! x = %u \n", x);
-        }
-    }
-  }
-
+  
   return 0;
 }
